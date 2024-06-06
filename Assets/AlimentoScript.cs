ac0db15bfa8b516031300439816ea893c0b9059b
PabloVives30 <48116412@est.ort.edu.ar>
2024-06-06 10:45:27 -0300

    FINISH

--- Assets/AlimentoScript.cs
+++ Assets/AlimentoScript.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AlimentoScript : MonoBehaviour
 {
+
+    public int valorAlimentario;
+
     // Start is called before the first frame update
     void Start()
     {
---
+++ Assets/PersonajeScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PersonajeScript : MonoBehaviour
+{
+
+    public int nivelHambre;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void tomarAlimento(int valorAlimentario)
+    {
+        nivelHambre = valorAlimentario;
+        if(nivelHambre <= 0)
+        {
+            nivelHambre = 0;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        AlimentoScript alimento;
+        alimento = other.GetComponent<AlimentoScript>();
+        if (alimento)
+        {
+            tomarAlimento(alimento.valorAlimentario);
+        }
+        Destroy(other.gameObject);
+    }
+}
